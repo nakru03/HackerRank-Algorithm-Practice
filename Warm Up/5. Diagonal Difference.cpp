938edeb2a1410d2b67249918ce916dbf0750319a
SangHyungKim <nakru03@gmail.com>
2018-07-15 20:11:49 +0900

    Solve Warm Up
    Warm up C++ Solution

---
+++ Warm Up/5. Diagonal Difference.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the diagonalDifference function below.
+int diagonalDifference(vector<vector<int>> arr) {
+    int sum_right = 0;
+    int sum_left = 0;
+    
+    for(int i=0; i<arr.size(); i++)
+    {
+        sum_right += arr[i][i];
+        sum_left += arr[i][(arr.size()-1)-i];        
+    }
+        
+    return abs(sum_right-sum_left);
+    
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<vector<int>> arr(n);
+    for (int i = 0; i < n; i++) {
+        arr[i].resize(n);
+
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    int result = diagonalDifference(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
